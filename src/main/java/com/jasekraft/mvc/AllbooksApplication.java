4c614a435ea7bd0f4e5ce20d493e0b61632bcf90
Jase Kraft <jase@perfectmypodcast.com>
2021-10-05 14:37:19 -0500

    REST application built for Java

---
+++ src/main/java/com/jasekraft/mvc/AllbooksApplication.java
@@ -0,0 +1,13 @@
+package com.jasekraft.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllbooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllbooksApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/jasekraft/mvc/services/BookService.java
@@ -0,0 +1,54 @@
+package com.jasekraft.mvc.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.jasekraft.mvc.models.Book;
+import com.jasekraft.mvc.repositories.BookRepository;
+
+@Service
+public class BookService {
+	private final BookRepository bookRepository;
+	
+	public BookService(BookRepository bookRepository) {
+		this.bookRepository = bookRepository;
+	}
+    // returns all the books
+    public List<Book> allBooks() {
+        return bookRepository.findAll();
+    }
+    // creates a book
+    public Book createBook(Book b) {
+        return bookRepository.save(b);
+    }
+    // retrieves a book
+    public Book findBook(Long id) {
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        if(optionalBook.isPresent()) {
+            return optionalBook.get();
+        } else {
+            return null;
+        }
+    }
+    public void deleteBook(long id) {
+    	bookRepository.deleteById(id);
+    }
+    
+    public Book updateBook(long id, String title, String desc, String lang, int pages) {
+    	Optional<Book> optionalBook = bookRepository.findById(id);
+    	if(optionalBook.isPresent()) {
+    		Book book = optionalBook.get();
+    		book.setTitle(title);
+    		book.setDescription(desc);
+    		book.setLanguage(lang);
+    		book.setNumberOfPages(pages);
+    		bookRepository.save(book);
+    		return book;
+    	}
+    	else {
+    		return null;
+    	}
+    }
+}
